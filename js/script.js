2d9a1723d96f32c59fabe666feee016f4512a76c
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-22 12:58:26 +0200

    Iniziando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready({
 
+// chiamata API https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+
+// API personal
+// 0dc314a9e6f7e554dbdb64c779cd9892
+
+
+
 });
