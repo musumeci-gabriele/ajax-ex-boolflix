c3584f7d7586cc1345c54f057146a0e05bac6007
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-22 16:47:26 +0200

    errore API

--- js/script.js
+++ js/script.js
@@ -2,43 +2,48 @@ $(document).ready(function(){
 
   var searchMovie = "ritorno al futuro";
 
+  // STAMPA A SCHERMO LE INFO DEL FILM
+  renderMovie(searchMovie);
+
+
+});
+
+// FUNZIONE DI STAMPA DELLE INFORMAZIONI DEL FILM
+function renderMovie(movies){
   // CHIAMATA AL SERVER PER LA RICERCA DELLE INFO RICHIESTE
   $.ajax(
     {
       "url": "https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false",
       "data": {
         "api_key": "0dc314a9e6f7e554dbdb64c779cd9892",
-        "query": searchMovie,
-        "language": it-IT
+        "query": "searchMovie",
+        "language": "it"
       },
       "method": "GET",
       "success": function(data){
-        console.log(data.result);
+        var moviesDb = data.result;
+
+        var source = $("#info-movie-template").html();
+        var template = Handlebars.compile(source);
+
+        // STAMPA INFO FILM
+        for (var i = 0; i <= moviesDb.lenght; i++) {
+          var context = {
+            "title": moviesDb[i].title,
+            "original_title": moviesDb[i].original_title,
+            "original_language": moviesDb[i].original_language,
+            "vote_average": moviesDb[i].vote_average
+          };
+
+          var html = template(context);
+
+          $("#movies-list").append(html);
+        }
       },
-      "error": function(){
+      "error" : function() {
         alert("Errore!");
       }
     }
   );
 
-});
-
-// FUNZIONE DI STAMPA DELLE INFORMAZIONI DEL FILM
-function renderMovie(movies){
-  var source = $("#info-movie-template").html();
-  var template = Handlebars.compile(source);
-
-  // STAMPA INFO FILM
-  for (var i = 0; i <= movies.lenght; i++) {
-    var context = {
-      "title": movies[i].title,
-      "original_title": movies[i].original_title,
-      "original_language": movies[i].original_language,
-      "vote_count": movies[i].vote_count
-    };
-
-    var html = template(context);
-
-    $("#movies-list").append(html);
-  }
 }
