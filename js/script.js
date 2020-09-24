211e32e8131f0fea64ed28c3e5e58ac1afd48557
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-24 16:17:44 +0200

    errore serie tv

--- js/script.js
+++ js/script.js
@@ -1,89 +1,132 @@
 $(document).ready(function(){
 
-  // FUNCTION STARS TO VOTE
+  // FUNCTION PRINT STARS
   function stars(vote){
-    var numStar = parseInt(Math.ceil((vote)/2));
-    var stars = '<i class="fas fa-star yellow"></i>';
+    var numStar = Math.ceil((vote)/2);
     var htmlStars ="";
-    for (var i = 0; i < numStar; i++) {
-      htmlStars += stars;
+
+    for (var i = 1; i <= 5; i++) {
+      // ciclo per la stampa delle stelle piene e vuote
+      if (i <= numStar){
+        htmlStars += '<i class="fas fa-star yellow"></i>';
+      } else {
+        htmlStars += '<i class="far fa-star yellow"></i>';
+      }
+
     }
     return htmlStars;
   }
 
   // FUNCTION FLAGS
-  var flags = ["es","de","fr","en","it"];
   function changeFlag(language){
+    var flags = ["es","de","fr","en","it"];
+
     if (flags.includes(language)) {
-      var imgFlag = '<img src ="img/'+ language +'.svg">';
-      // imgFlag.addClass("img-flag");
-      return imgFlag;
+      return "<img class='img-flag' src='img/"+language+".svg'>";
     }
-    else{
-      return language;
-    }
+
+    return language;
   };
 
-
   // FUNCTION CALL TO SERVER FOR INFO ABOUT THE MOVIE
-  function renderMovie(movies){
+  function renderMovie(searchMovie){
 
-      if (searchMovie != ""){
-        $(".input-title").val("");
-        $("#movies-list").html("");
-
-        // RECALL TO SERVER FOR INFO ABOUT THE MOVIE
-        $.ajax(
-          {
-            "url" : "https://api.themoviedb.org/3/search/movie",
-            "data" : {
-              "api_key": "0dc314a9e6f7e554dbdb64c779cd9892",
-              "query": searchMovie,
-              "language": "it"
-            },
-            "method" : "GET",
-            "success" : function(data){
-              infoSuccess(data.results);
-            },
-            "error" : function() {
-              alert("Errore!");
-            }
-          }
-        );
-      } else {
-        alert("Non hai scritto nulla!");
+    $.ajax(
+      {
+        "url" : "https://api.themoviedb.org/3/search/movie",
+        "data" : {
+          "api_key": "0dc314a9e6f7e554dbdb64c779cd9892",
+          "query": searchMovie,
+          "language": "it"
+        },
+        "method" : "GET",
+        "success" : function(data){
+          infoSuccess("Film", data.results);
+        },
+        "error" : function() {
+          alert("Errore!");
+        }
       }
+    );
+
+  }
+
+
+  // FUNCTION CALL TO SERVER FOR INFO ABOUT THE SERIES TV
+  function renderSeries(searchSeries){
+
+    $.ajax(
+      {
+        "url" : "https://api.themoviedb.org/3/search/tv",
+        "data" : {
+          "api_key": "0dc314a9e6f7e554dbdb64c779cd9892",
+          "query": searchSeries,
+          "language": "it"
+        },
+        "method" : "GET",
+        "success" : function(data){
+          infoSuccess("Serie Tv", data.results);
+        },
+        "error" : function() {
+          alert("Errore!");
+        }
+      }
+    );
+
   }
 
   //  FUNCTION HABDLEBARS
-  function infoSuccess(movies){
+  function infoSuccess(type, results){
 
     // LINK TO HANDLEBARS
     var source = $("#info-movie-template").html();
     var template = Handlebars.compile(source);
 
     // INFO ABOUT THE MOVIE
-    for (var i = 0; i < movies.length; i++) {
+    for (var i = 0; i < ((type, results).length); i++) {
+
+      var title, original_title;
+
+      if(type == "Film"){
+        title = results[i].title;
+        original_title = results[i].original_title;
+      } else if(type == "Serie tv"){
+        title = results[i].name;
+        original_title = results[i].original_name;
+      }
 
       var context = {
-        "title": movies[i].title,
-        "original_title": movies[i].original_title,
-        "original_language": changeFlag(movies[i].original_language),
-        "vote_average": stars(movies[i].vote_average)
+        "title": title,
+        "original_title": original_title,
+        "original_language": changeFlag(results[i].original_language),
+        "vote_average": stars(results[i].vote_average),
+        "type": type
       };
 
       var html = template(context);
 
-      $("#movies-list").append(html);
+      $("#serch-list").append(html);
+    }
+  }
+
+  // FUNCTION CLEAR INPUT
+  function clearInput(){
+    if (searchMovie != ""){
+      $(".input-title").val("");
+      $("#search-list").html("");
+    } else {
+      alert("Non hai scritto nulla!");
     }
   }
 
   // EVENT CLICK ON SEARCH BUTTON
   $(".btn-search").click(
     function(){
-      searchMovie = $(".input-title").val();
-      // PRINT INFO ABOUT THE MOVIE
-      renderMovie(searchMovie);
+      var search = $(".input-title").val();
+      // PRINT INFO ABOUT THE SEARCH
+      renderMovie(search);
+      renderSeries(search);
+
     }
   );
 
@@ -91,9 +134,13 @@ $(document).ready(function(){
   $(".input-title").keydown(
     function(event) {
       if (event.which ==13){
-        searchMovie = $(".input-title").val();
-        // STAMPA A SCHERMO LE INFO DEL FILM
-        renderMovie(searchMovie);
+        var search = $(".input-title").val();
+
+        // PRINT INFO ABOUT THE SEARCH
+        renderMovie(search);
+        renderSeries(search);
+
+
       }
     }
   );
